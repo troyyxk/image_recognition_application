f3eb2de77ab835a45bbf6da65dc626189195ed9f
troyyxk <xingkunyin@hotmail.com>
2021-11-23 02:04:16 -0500

    Update

--- front_end/image_recognition_frontend/src/components/fileUploader.jsx
+++ front_end/image_recognition_frontend/src/components/fileUploader.jsx
@@ -2,31 +2,29 @@ import React, { Component } from 'react';
 import {useState} from 'react';
 import '../fileUploader.css';
 
-export const FilerUploader = ({}) => {
-    const [file, setFile] = useState(null);
-
-    const onInputChange = (e) => {
+class FilerUploader extends Component {
+    onInputChange = (e) => {
         console.log(e.target.files);
         console.log(e.target.files[0]);
-        setFile(e.target.files[0]);
         console.log(e.target.value);
     }
-    
-    const onSubmit = (e) => {
+
+    onSubmit = (e) => {
         e.preventDefault();
     }
 
-    // state = {}
-    return(
-        <div class="col-md-6">
-            <form method="post" action="#" id="#" onSubmit={onSubmit}>
-                <div class="form-group files color">
-                    <label>Upload Your File </label>
-                    <input type="file" onChange={onInputChange} class="form-control" multiple=""/>
-                </div>
-            </form>
-        </div>
-    );
+    render() {
+        return(
+            <div class="col-md-6">
+                <form method="post" action="#" id="#" onSubmit={this.onSubmit}>
+                    <div class="form-group files color">
+                        <label>Upload Your File </label>
+                        <input type="file" onChange={this.onInputChange} class="form-control" multiple=""/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default FilerUploader;
