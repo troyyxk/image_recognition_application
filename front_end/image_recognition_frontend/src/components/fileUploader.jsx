d738ae6155a52a084741da986f02052e23f26241
troyyxk <xingkunyin@hotmail.com>
2021-11-23 02:23:01 -0500

    Update

--- front_end/image_recognition_frontend/src/components/fileUploader.jsx
+++ front_end/image_recognition_frontend/src/components/fileUploader.jsx
@@ -4,9 +4,16 @@ import '../fileUploader.css';
 
 class FilerUploader extends Component {
     onInputChange = (e) => {
+        // when submit new files, clear the records the old ones
+        window.localStorage.clear();
+        for (let i = 0; i < e.target.files.length; i++) {
+            window.localStorage.setItem(e.target.files[i].name, "");
+        }
         console.log(e.target.files);
         console.log(e.target.files[0]);
         console.log(e.target.value);
+        // window.localStorage.setItem();
+        console.log(window.localStorage);
     }
 
     onSubmit = (e) => {
@@ -19,7 +26,7 @@ class FilerUploader extends Component {
                 <form method="post" action="#" id="#" onSubmit={this.onSubmit}>
                     <div class="form-group files color">
                         <label>Upload Your File </label>
-                        <input type="file" onChange={this.onInputChange} class="form-control" multiple=""/>
+                        <input type="file" onChange={this.onInputChange} class="form-control" multiple="true"/>
                     </div>
                 </form>
             </div>
