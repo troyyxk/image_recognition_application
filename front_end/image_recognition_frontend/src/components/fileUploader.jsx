b39a343a8d65dcb47817e3190ed564077f350ee9
troyyxk <xingkunyin@hotmail.com>
2021-11-28 20:21:08 -0500

    Add filed git push

--- front_end/image_recognition_frontend/src/components/fileUploader.jsx
+++ front_end/image_recognition_frontend/src/components/fileUploader.jsx
@@ -5,22 +5,28 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 toast.configure();
 
+// reference
+// https://stackoverflow.com/questions/28918232/how-do-i-persist-a-es6-map-in-localstorage-or-elsewhere
+
 class FilerUploader extends Component {
     onInputChange = (e) => {
         // when submit new files, clear the records the old ones
         if (window.localStorage.getItem("irImage") !== null) {
             window.localStorage.removeItem("irImage");
         }
-        var irImage = {};
+        var irImage = [];
+        var nameSet = new Set();
         for (let i = 0; i < e.target.files.length; i++) {
-            if (window.localStorage.getItem(e.target.files[i].name) !== null) {
+            if (nameSet.has(e.target.files[i].name)) {
                 toast.error('Cannot submit files with the same name!');
-                window.localStorage.clear();
+                window.localStorage.removeItem("irImage");
                 return;
             }
-            window.localStorage.setItem(e.target.files[i].name, "");
+            nameSet.add(e.target.files[i].name);
+            irImage.push({name: e.target.files[i].name, uuid: "", result: ""});
         }
         toast.success('Files have been successfully uploaded! Please turn to review tab for checking the result.');
+        window.localStorage.setItem("irImage", JSON.stringify(irImage));
         console.log(window.localStorage);
     }
 
