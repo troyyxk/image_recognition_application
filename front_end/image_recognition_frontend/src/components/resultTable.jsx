2e4126f8925cfba7048fd0bccdfed34b40ddc7db
troyyxk <xingkunyin@hotmail.com>
2021-11-29 23:52:44 -0500

    Fix async proglem

--- front_end/image_recognition_frontend/src/components/resultTable.jsx
+++ front_end/image_recognition_frontend/src/components/resultTable.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Container,Row, Button, Table } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
 import { getResult } from '../services/imageService';
 import '../App.css';
 import Submit from './submit';
@@ -16,9 +17,49 @@ class ResultTable extends Component {
     handleOnClick = () => {
         for (var key in Object.keys(this.state.irImage)) {
             var uuid = this.state.irImage[key].uuid;
-            getResult(uuid);
+            var result = getResult(uuid);
+            result
+            .then((response) => {
+                // console.log(response);
+                if (response.status === 200) {
+                    // console.log(response.data)
+                    var status = response.data.result.status;
+                    var result;
+                    var irImage = [];
+                    var ogIrImage = JSON.parse(window.localStorage.getItem("irImage"));
+                    var i = 0;
+                    for (var key in Object.keys(ogIrImage)) {
+                        if (ogIrImage[key].uuid === uuid) {
+                            if (status === 0) {
+                                result = "Status: Waiting"
+                            }
+                            else if (status === 1) {
+                                result = "Status: Running"
+                            }
+                            else if (status === 2) {
+                                result = response.data.result.category;
+                            }
+                            else if (status === 3) {
+                                result = "Status: Failed"
+                            }
+                        }
+                        irImage.push({serialNum: ogIrImage[key].serialNum, name: ogIrImage[key].name, uuid: ogIrImage[key].uuid, result: result});
+                        i++;
+                    }
+                    console.log("in getResult")
+                    console.log(irImage);
+                    window.localStorage.setItem("irImage", JSON.stringify(irImage));
+                    this.setState({irImage: irImage});
+                } else {
+                    toast.error('Upload image fail!');
+                    console.error("Upload image fail!");
+                    return false;
+                }
+            })
+            .catch((e) => {
+                console.error("Error", e);
+            })
         }
-        window.location.reload();
     }
 
     render() {
@@ -30,7 +71,6 @@ class ResultTable extends Component {
                 );
         }
         
-        console.log(this.state.irImage)
         return (
             <div className="App-header">
                 <Button variant="info" onClick={this.handleOnClick}>Refresh</Button>
--- front_end/image_recognition_frontend/src/services/imageService.js
+++ front_end/image_recognition_frontend/src/services/imageService.js
@@ -18,14 +18,14 @@ export const postImage = (img, name) => {
                     }
                     i++;
                 }
-                console.log(irImage);
+                // console.log(irImage);
                 window.localStorage.setItem("irImage", JSON.stringify(irImage));
             } else {
                 toast.error('Upload image fail!');
                 console.error("Upload image fail!");
                 return false;
             }
-            console.log(response);
+            // console.log(response);
         })
         .catch((e) => {
             console.error("Error", e);
@@ -34,46 +34,8 @@ export const postImage = (img, name) => {
 }
 
 export const getResult = (uuid) => {
-    console.log("uuid")
-    console.log(uuid)
-    axios.get("/api/query/" + uuid)
-        .then((response) => {
-            console.log(response);
-            if (response.status === 200) {
-                console.log(response.data)
-                var status = response.data.result.status;
-                var result;
-                var irImage = [];
-                var ogIrImage = JSON.parse(window.localStorage.getItem("irImage"));
-                var i = 0;
-                for (var key in Object.keys(ogIrImage)) {
-                    if (ogIrImage[key].uuid === uuid) {
-                        if (status === 0) {
-                            result = "Status: Waiting"
-                        }
-                        else if (status === 1) {
-                            result = "Status: Running"
-                        }
-                        else if (status === 2) {
-                            result = response.data.result.category;
-                        }
-                        else if (status === 3) {
-                            result = "Status: Failed"
-                        }
-                    }
-                    irImage.push({serialNum: ogIrImage[key].serialNum, name: ogIrImage[key].name, uuid: ogIrImage[key].uuid, result: result});
-                    i++;
-                }
-                console.log(irImage);
-                window.localStorage.setItem("irImage", JSON.stringify(irImage));
-            } else {
-                toast.error('Upload image fail!');
-                console.error("Upload image fail!");
-                return false;
-            }
-        })
-        .catch((e) => {
-            console.error("Error", e);
-        })
-    return true;
+    // console.log("uuid")
+    // console.log(uuid)
+    var result = axios.get("/api/query/" + uuid + '/')
+    return result;
 }
