712d7a0bca151d4a42161e5c69905ff15eb0b4e5
wenyi999 <2947711395@qq.com>
2021-12-06 18:54:20 -0500

    add router for distributed worker

--- back_end/back_end/back_end/celery.py
+++ back_end/back_end/back_end/celery.py
@@ -6,8 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_end.settings')
 
 app = Celery('back_end', broker=os.getenv('amqp://guest@localhost:5672//'))
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
 app.autodiscover_tasks()
+app.conf.task_routes = {'app.tasks.recognize_image': {'queue': 'recognize_image'}}
+#app.conf.task_routes = {'app.tasks.recognize_image': {'queue': 'recognize_image'}}
+#app.conf.task_routes = {'app.tasks.recognize_image': {'queue': 'recognize_image'}}
+#app.conf.task_routes = {'app.tasks.recognize_image': {'queue': 'recognize_image'}}
+#app.conf.task_routes = {'app.tasks.recognize_image': {'queue': 'recognize_image'}}
+#app.conf.task_routes = {'app.tasks.recognize_image': {'queue': 'recognize_image'}}
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
