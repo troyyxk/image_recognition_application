e131edb44f99fff86771e3582dd75e1919115d62
Simon <youyi@shanghaitech.edu.cn>
2021-12-07 17:46:33 -0500

    add rabbitmq

--- setup.sh
+++ setup.sh
@@ -12,6 +12,15 @@ if [ $role = "1" ]; then
 
   sudo apt-get update
   sudo apt-get install -y python3-pip libjpeg8-dev zlib1g-dev nginx
+  sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  echo "Installing docker..."
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
 
   echo "Installing Django and Dependencies..."
   sudo pip3 install django pillow celery
@@ -34,7 +43,12 @@ if [ $role = "1" ]; then
   sudo cp $BASE_DIR/server.conf /etc/nginx/sites-enabled/
   sudo rm /etc/nginx/sites-enabled/default
 
+  echo "Reload nginx server..."
   sudo systemctl restart nginx
+
+  echo "Starting RabbitMQ..."
+  sudo docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
+
 
 elif [ $role = "2" ]; then
   echo "hello"
