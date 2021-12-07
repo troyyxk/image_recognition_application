728c5b615793b61f34a7200df62419743bb1f823
Simon <youyi@shanghaitech.edu.cn>
2021-12-07 18:28:12 -0500

    update setup.sh for worker

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ elif [ $role = "2" ]; then
 
   echo "Installing dependencies..."
   sudo apt-get update
-  sudo apt-get install -y python3-pip libjpeg8-dev zlib1g-dev
+  sudo apt-get install -y python3-pip libjpeg8-dev zlib1g-dev celery
   pip3 install setuptools
   pip3 install wheel
   pip3 install torch==1.10.0+cpu torchvision==0.11.1+cpu torchaudio==0.10.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
