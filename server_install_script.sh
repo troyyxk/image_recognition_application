9b0094896025f11009817667d24a4f7ca2bb6842
troyyxk <xingkunyin@hotmail.com>
2021-12-07 20:53:34 -0500

    UPdate

--- server_install_script.sh
+++ server_install_script.sh
@@ -1,3 +1,4 @@
-﻿
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
 pip3 install amqp==5.0.6 asgiref==3.4.1 billiard==3.6.4.0 cached-property==1.5.2 celery==5.1.2 click==7.1.2 click-didyoumean==0.3.0 click-plugins==1.1.1 click-repl==0.2.0 Django==3.2.9 django-celery-results==2.2.0 importlib-metadata==4.8.2 kombu==5.1.0 Pillow==8.4.0 prompt-toolkit==3.0.23 pytz==2021.3 six==1.16.0 sqlparse==0.4.2 typing-extensions==4.0.0 vine==5.0.0 wcwidth==0.2.5 zipp==3.6.0
